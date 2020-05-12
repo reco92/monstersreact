84350200cf0685b3c4423003dac935e91370c29d
Rodrigo Corrales <rodrigocorrales@amigocloud.com>
2020-05-12 18:15:13 -0500

    Updates

---
+++ precache-manifest.e890639e6a8113b525b7179b67fad145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997507d2d2ddee3fb8e08dfcd6572903",
+    "url": "/monstersreact/index.html"
+  },
+  {
+    "revision": "640258ef69c088a1256b",
+    "url": "/monstersreact/static/css/main.f5f88ade.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monstersreact/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersreact/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640258ef69c088a1256b",
+    "url": "/monstersreact/static/js/main.56fb7d3e.chunk.js"
+  },
+  {
+    "revision": "f475bd0d5d65fb243602",
+    "url": "/monstersreact/static/js/runtime-main.ddd6d17f.js"
+  }
+]);
